569d31183576974f702b3722992b2be60152242a
Rafael-Martins-Paiva <rafael.martins.paiva0@gmail.com>
2024-11-30 19:47:00 -0300

    update plugin3.js

--- test/plugins/plugin3.js
+++ test/plugins/plugin3.js
@@ -7,4 +7,5 @@ module.exports.info = {
 module.exports.activate = () => {
   const colors = ["#FF5733", "#33FF57", "#3357FF", "#F333FF", "#FF8C33"];
   document.body.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+  alert('ola mundo')
 };
